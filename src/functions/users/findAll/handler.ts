34102bafb07a94cb5f1164706bfc03a1b8fcb0c2
Roberto Berrocal <robertoberro20@gmail.com>
2023-07-22 13:18:31 -0500

    first commit

---
+++ src/functions/users/findAll/handler.ts
@@ -0,0 +1,16 @@
+import { formatJSONResponse } from '@libs/api-gateway';
+import { container } from 'src/config/inversify.config';
+import { UserService } from 'src/users/services/user.service';
+
+export const main = async () => {
+  try {
+    const userService = container.get(UserService);
+    return formatJSONResponse({
+      result: userService.findAll(),
+    });
+  } catch (error) {
+    return formatJSONResponse({
+      error: error.message,
+    });
+  }
+};
---
+++ src/users/mock/find.all.mock.ts
@@ -0,0 +1,5 @@
+export const findAllMock = [
+  { id: 1, fullName: 'Roberto', age: 28 },
+  { id: 2, fullName: 'Carlos', age: 21 },
+  { id: 3, fullName: 'Luis', age: 22 },
+];
